0d657cf394460dc8dcdd66e090b1d7136786b471
jodbsgh <jodbsgh@gmail.com>
2022-03-16 16:18:45 +0900

    [#14]note 완성
    note_list 구현
    note_view 구현
    note_write 구현
    
    
    MemberController.java 내용 추가
    MemberController
    @RequestMapping(value="/member/login_proc")
    - session.setAttribute("userLevel", resultDto.getUser_level());    //로그인시 userLevel 데이터,  session에 추가
    
    
    
    nav.jsp 내용 추가
    String userLevel=StringUtil.nullToValue(session.getAttribute("userLevel"),"");   //session 에서 userLevel 데이터 받아오기

--- src/main/java/com/woori/yourhome/board/BoardService.java
+++ src/main/java/com/woori/yourhome/board/BoardService.java
@@ -11,9 +11,10 @@ public interface BoardService {
 	int getTotal(BoardDto dto);
 	void delete(String id);
 	void update(BoardDto dto);
+	
+	
 	void comment_insert(CommentDto dto);
 	List<CommentDto> getCommentList(CommentDto dto);
-	
 	CommentDto getCommentView(CommentDto dto);
 	void comment_update(CommentDto dto);
 	void comment_delete(CommentDto dto);
